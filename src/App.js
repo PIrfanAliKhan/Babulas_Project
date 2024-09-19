91091b694d1d827a46516754ec38bd384ef664c4
PIrfanAliKhan <irfanalikhanp9@gmail.com>
2024-09-19 11:31:29 +0530

    undeted

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import Projects from './Projects/Projects';
 import Aboutus from './Aboutus/Aboutus';
 import Contactus from './ContactUs/Contactus';
 import Services from './Services/Services';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
+    
+    <>
+    {/* <Router> */}
     <div className="App">
       <Navbar />
       <Aboutus/>
@@ -15,7 +20,22 @@ function App() {
       <Projects/>
       <Services/>
       <Contactus/>
+      
+      {/* <link to="">Navbar</link>
+      <link to="/projects">Projects</link>
+      <link to="/services">Services</link>
+      <link to="/contact">ContactUs</link>
+    
+        <Routes>
+          <Route path='' element={<Navbar/>}></Route>
+          <Route path='/projects' element={<Projects/>}></Route>
+          <Route path='/services' element={<Services/>}></Route>
+          <Route path='/contact'element={<Contactus/>}></Route>
+        </Routes>
+      
+    </Router> */}
     </div>
+    </>
   );
 }
 
