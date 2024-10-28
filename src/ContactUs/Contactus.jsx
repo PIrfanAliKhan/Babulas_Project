848d23a7586b02a7e556310fec1637c5ffa0da3e
PIrfanAliKhan <irfanalikhanp9@gmail.com>
2024-10-28 21:30:58 +0530

    contact undated

--- src/ContactUs/Contactus.jsx
+++ src/ContactUs/Contactus.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './Contact.css'
+// Contactus.js
+import React from 'react';
+import './Contact.css';
 
 function Contactus() {
   return (
     <div className='contact'>
-      <div className="contactleft">
-        <p>contact</p>
-        <h2>AISLIN </h2>
-        <h4>Dream to reality</h4>
+      <div className="contact-left">
+        <p className="contact-title"><h2>CONTACT</h2></p>
+        <h2 className="contact-brand">AISLIN</h2>
+        <h4 className="contact-slogan">Dream to Reality</h4>
       </div>
-      <div className="contactright">
-        <h4>whatsapp:<h6>9154328747</h6></h4>
-        <h4>Email: <h6>irfanalikhanp9@gmail.com</h6></h4>
-        <h4>facebook: <h6>irfanalikhan</h6></h4>
+      <div className="contact-right">
+        <div className="contact-detail">
+          <span className="contact-label">WhatsApp:</span>
+          <span className="contact-info">9154328747</span>
+        </div>
+        <div className="contact-detail">
+          <span className="contact-label">Email:</span>
+          <span className="contact-info">irfanalikhanp9@gmail.com</span>
+        </div>
+        <div className="contact-detail">
+          <span className="contact-label">Facebook:</span>
+          <span className="contact-info">irfanalikhan</span>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contactus
+export default Contactus;
